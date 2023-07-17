c425841c14ce0b5b8ea3f30dfbdc310caac212e9
Saket <saketagrahari.nita@gmail.com>
2023-07-17 13:54:15 +0530

    first commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,37 @@
+import { Form } from "react-router-dom"
+
+// library
+import { UserPlusIcon } from "@heroicons/react/24/solid";
+
+// assets
+import illustration from "../assets/il1.jpg"
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div>
+        <h2 className="personalized">
+        Your <span className="accentper">Personalized</span> Budget Planner
+        </h2>
+        <p className="descrip">
+        Easily track your expenses and savings to make informed financial decisions and achieve your money goals.
+        </p>
+        <Form method="post">
+          <input
+            type="text"
+            name="userName"
+            required
+            placeholder="Name" aria-label="Your Name" autoComplete="given-name"
+          />
+          <input type="hidden" name="_action" value="newUser" />
+          <button type="submit" className="btn btn--dark">
+            <span>Create Account</span>
+            <UserPlusIcon width={20} />
+          </button>
+        </Form>
+      </div>
+      <img className="ilimg" src={illustration} alt="Person with money" width={600} />
+    </div>
+  )
+}
+export default Intro
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+    <div className="footer">
+        <p>Made with love 🤍 by Saket Agrahari</p>
+    </div>
+);
+
+export default Footer;
