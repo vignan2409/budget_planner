b19ade8c26be1593facf9bf8fad1825979ab7094
Saket <saketagrahari.nita@gmail.com>
2023-08-21 17:44:43 +0530

    Add explanatory comments
    Fixes #03

--- src/actions/deleteBudget.js
+++ src/actions/deleteBudget.js
@@ -1,14 +1,18 @@
-// rrd import
+//* rrd import
+// This function is likely used to navigate the user to a different route after the budget is 
+// successfully deleted.
 import { redirect } from "react-router-dom";
 
-// library
+//* library
+// The toast object is used to display notifications or toasts to the user.
 import { toast } from "react-toastify";
 
-// helpers
+//* helpers
 import { deleteItem, getAllMatchingItems } from "../helpers";
 
 export function deleteBudget({ params }) {
   try {
+    // call the deleteItem function with an object as an argument
     deleteItem({
       key: "budgets",
       id: params.id,
@@ -20,6 +24,8 @@ export function deleteBudget({ params }) {
       value: params.id,
     });
 
+    // iterate through the array associatedExpenses and for each expense, call the deleteItem 
+    // function to delete the expense.
     associatedExpenses.forEach((expense) => {
       deleteItem({
         key: "expenses",
@@ -27,9 +33,13 @@ export function deleteBudget({ params }) {
       });
     });
 
+    // use the toast object to display a success notification 
     toast.success("Budget deleted successfully!");
   } catch (e) {
     throw new Error("There was a problem deleting your budget.");
   }
+
+  // direct the user to the root route ("/") after the budget and its associated expenses 
+  // have been successfully deleted.
   return redirect("/");
 }
