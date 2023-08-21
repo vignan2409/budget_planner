21675789b94f614fa6eced356d0fa259642ca658
Saket <saketagrahari.nita@gmail.com>
2023-08-21 18:56:31 +0530

    Add explanatory comments
    Added inline comments to clarify the purpose and behavior of
    each step. This will make it easier for other developers to understand the codebase.
    
    Fixes #003

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -11,18 +11,21 @@ import Nav from "../components/Nav";
 import { fetchData } from "../helpers"
 
 // loader
+// intended to be used as a data loader for the route.
 export function mainLoader() {
-  const userName = fetchData("userName");
+  const userName = fetchData("userName");   // fetchData is called to retrieve user data.
   return { userName }
 }
 
 const Main = () => {
-  const { userName } = useLoaderData()
+  const { userName } = useLoaderData()   // used to access the data loaded by the mainLoader function
 
   return (
     <div className="layout">
       <Nav userName={userName} />
       <main>
+        {/* serves as a placeholder for nested routes. This allows the rendering of child 
+        components based on the route structure. */}
         <Outlet />
       </main>
     </div>
