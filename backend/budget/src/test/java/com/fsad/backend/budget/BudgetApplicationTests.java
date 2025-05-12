a7c401cf2f93ed8dc7ed8c08893ce60df5b433cf
vignan2409 <163747556+vignan2409@users.noreply.github.com>
2025-05-12 11:32:05 +0530

    Add files via upload

---
+++ backend/budget/src/test/java/com/fsad/backend/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsad.backend.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
